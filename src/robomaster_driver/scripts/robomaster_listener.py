957c168091ffd1d8d7cad2d16910efcc65bbcdaf
Erdem Uysal <erdemuysal@yahoo.com>
2021-09-25 23:03:56 +0300

    pub/sub methodology has been changed

--- src/robomaster_driver/scripts/robomaster_listener.py
+++ src/robomaster_driver/scripts/robomaster_listener.py
@@ -1,51 +1,51 @@
 #!/usr/bin/env python
 
 import rospy
-from robomaster_msgs import TwistAccel, GimbalAngle
-from robomaster import robot, chassis, gimbal, led
+from robomaster_msgs.msg import TwistAccel, GimbalAngle
+from robomaster import robot, led
 
 
-class RobomasterListener:
-    def __init__(self, gimbalMode=True, chassisMode=False ):
-        self.robot = robot.Robot()  # Robomaster robot instance
-        self.robot.initialize(conn_type="sta")
-        self.chassis = self.robot.chassis
-        self.gimbal = self.robot.gimbal
-        self.led = self.robot.led
-        self.twist_accel = None
-        self.gimbal_angle = None
-        self.chassisMode = chassisMode
-        self.gimbalMode = gimbalMode
-        self.velocity_sub = rospy.Subscriber('/robomaster/chassis', TwistAccel, self.chassis_callback)
-        self.gimbal_sub = rospy.Subscriber('/robomaster/gimbal', GimbalAngle , self.gimbal_callback)
+class RobomasterListener(robot.Robot):
+    def __init__(self, cli=None):
+        super().__init__(cli=cli)
+        self.initialize(conn_type="sta")
+        self.robomaster_ip = rospy.set_param('~robomaster/ip', '192.168.X.X')
+        self.robomaster_version = rospy.set_param('~robomaster/version', self.get_version())
+        self.robomaster_sn = rospy.set_param('~robomaster/sn', self.get_sn())
+        self.chassis_mode = rospy.set_param('~robomaster/chassis_mode', True)
+        self.gimbal_mode = rospy.set_param('~robomaster/gimbal_mode', False)
+        self.chassis_msg = TwistAccel()
+        self.gimbal_msg = GimbalAngle()
+        self.sub_chassis = rospy.Subscriber('/robomaster/chassis', TwistAccel, self.cb_chassis)
+        self.sub_gimbal = rospy.Subscriber('/robomaster/gimbal', GimbalAngle , self.cb_gimbal)
 
-    def chassis_callback(self, msg):
-        self.twist_accel = msg
-
-        if self.chassisMode == False :
-            self.robot.set_robot_mode(mode=robot.CHASSIS_LEAD)
-            self.chassisMode = True
-            self.gimbalMode = False
-
-        self.chassis.drive_speed(x=self.twist_accel.twist.linear.x, y=0, z=self.twist_accel.twist.angular.z, timeout=5)
+    def cb_chassis(self, msg):
+        rospy.loginfo("[INFO] Subscribed to the chassis")
+        if self.chassis_mode == False :
+            self.set_robot_mode(mode=robot.CHASSIS_LEAD)
+            self.chassis_mode = True
+            self.gimbal_mode = False
+        self.chassis_msg = msg
+        self.chassis.drive_speed(x=self.chassis_msg.twist.linear.x, y=0, z=self.chassis_msg.twist.angular.z, timeout=5)
         self.led.set_led(comp=led.COMP_ALL, r=255, g=0, b=0, effect=led.EFFECT_ON)
 
-    def gimbal_callback(self, msg):
-        self.gimbal_angle = msg
-
-        if self.gimbalMode == False:
-            self.robot.set_robot_mode(mode=robot.GIMBAL_LEAD)
-            self.chassisMode = False
-            self.gimbalMode = True
-        
-        self.gimbal.drive_speed(pitch_speed=self.gimbal_angle.pitch, yaw_speed=self.gimbal_angle.yaw)
+    def cb_gimbal(self, msg):
+        rospy.loginfo("[INFO] Subscribed to the gimbal")
+        if self.gimbal_mode == False:
+            self.set_robot_mode(mode=robot.GIMBAL_LEAD)
+            self.chassis_mode = False
+            self.gimbal_mode = True
+        self.gimbal_msg = msg
+        self.gimbal.drive_speed(pitch_speed=self.gimbal_msg.pitch_angle, yaw_speed=self.gimbal_msg.yaw_angle)
         self.led.set_led(comp=led.COMP_ALL, r=0, g=255, b=0, effect=led.EFFECT_ON)
-
 
 if __name__ == '__main__':
     # Initialize ROS node
+    print('Initializing robomaster_listener node')
     rospy.init_node('robomaster_listener')
+    print('Navigating to robomaster_listener node')
     # Create an instance from listener class
     robomaster_listener = RobomasterListener()
     # Take a loop until an interruption is made
     rospy.spin()
+    robomaster_listener.close()
--- src/robomaster_driver/scripts/robomaster_talker.py
+++ src/robomaster_driver/scripts/robomaster_talker.py
@@ -1,95 +1,145 @@
 #!/usr/bin/env python
 
+import time
 import rospy
-from sensor_msgs.msg import Image
-from robomaster_msgs.msg import ImuAccGyro
+from std_msgs.msg import String, Float64
+from geometry_msgs.msg import Quaternion
+from sensor_msgs.msg import Image, Imu
 from cv_bridge import CvBridge, CvBridgeError
 from robomaster import robot
 
 
-class RobomasterTalker():
-    def __init__(self):
-        self.robot = robot.Robot()  # Robomaster robot instance
-        self.robot.initialize(conn_type="sta")
-        self.chassis = self.robot.chassis
-        self.camera = self.robot.camera
-        self.bridge = CvBridge()
-        self.img_pub = rospy.Publisher('/robomaster/image', Image, queue_size=1)
-        self.imu_pub = rospy.Publisher('/robomaster/imu', ImuAccGyro, queue_size=10)
-        self.imu_data_handler = self.chassis.sub_imu(freq=5, callback=self.set_imu_data)
-        self.rate = rospy.Rate(10)
+class RobomasterTalker(robot.Robot):
+    def __init__(self, cli=None):
+        super().__init__(cli=cli)
+        self.initialize(conn_type="sta")
+        self.image_msg = Image()
+        self.imu_msg = Imu()
+        self.attitude_msg = Quaternion()
+        self.esc_msg = String()
+        self.battery_msg = Float64()
+        self.image_pub = rospy.Publisher('/robomaster/image', Image, queue_size=1)
+        self.imu_pub = rospy.Publisher('/robomaster/imu', Imu, queue_size=10)
+        self.attitude_pub = rospy.Publisher('/robomaster/attitude', Quaternion, queue_size=10)
+        self.esc_pub = rospy.Publisher('/robomaster/esc', String, queue_size=10)
+        self.battery_pub = rospy.Publisher('/robomaster/battery', Float64, queue_size=10)
+        # Publish image data
+        self.get_image_msg()
+        time.sleep(3)
+        # Publish imu data
+        self.chassis.sub_imu(freq=1, callback=self.get_imu_msg)
+        time.sleep(3)
+        self.chassis.unsub_imu()
+        # Publish attitude data
+        self.chassis.sub_attitude(freq=1, callback=self.get_attitude_msg)
+        time.sleep(3)
+        self.chassis.unsub_attitude()
+        # Publish esc data
+        self.chassis.sub_esc(freq=1, callback=self.get_esc_msg)
+        time.sleep(3)
+        self.chassis.unsub_esc()
+        # Publish battery data
+        self.battery.sub_battery_info(1, self.get_battery_msg, robot.Robot())
+        time.sleep(3)
+        self.battery.unsub_battery_info()
+        # Get status parameters
+        self.chassis.sub_status(freq=1, callback=self.get_status_params)
+        time.sleep(3)
+        self.chassis.unsub_status()
 
     def get_image_msg(self):
-        """ 
-        Converts image data from OpenCV image to ROS message
-
-        returns:
-
-        image_msg -- ROS image message for image data 
         """
+        Gets image data from Robomaster's Python SDK,
+        converts it from OpenCV image to a ROS message and
+        then publishes converted message to the relevant topic
+        """
+        rospy.loginfo("Importing image data from robomaster")
         self.camera.start_video_stream(display=False)
-        for i in range(0, 200):
-            img = self.camera.read_cv2_image()
-            try:
-                image = self.bridge.cv2_to_imgmsg(img, "bgr8")
-                self.camera.stop_video_stream()
-                return image
-            except CvBridgeError as e:
-                print(e)
+        try:
+            self.image_msg = CvBridge().cv2_to_imgmsg(self.camera.read_cv2_image(), "bgr8")
+            self.image_pub.publish(self.image_msg)
+        except CvBridgeError as e:
+            print(e)
+        self.camera.stop_video_stream()
 
-    def set_imu_data(self, imu_data):
-        """ 
-        Get imu data from Robomaster Python SDK
-        
+    def get_imu_msg(self, imu_data):
+        """
+        Gets imu data from Robomaster's Python SDK,
+        convers that to a ROS message and then
+        publishes converted message to the relevant topic
+
         args:
-        imu_data -- imu data
-
-        returns:
-        acc_x -- accelaration on x axis
-        acc_y -- accelaration on y axis
-        acc_z -- accelaration on z axis
-        gyro_x -- gyro on x axis
-        gyro_y -- gyro on y axis
-        gyro_z -- gyro on z axis
+        imu_data -- imu data that comes from robomaster's subscriber method
         """
-        acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z = imu_data,
-        return acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z
+        rospy.loginfo("Importing imu data from robomaster")
+        acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z = imu_data
+        self.imu_msg.linear_acceleration.x = acc_x
+        self.imu_msg.linear_acceleration.y = acc_y
+        self.imu_msg.linear_acceleration.z = acc_z
+        self.imu_msg.orientation.x = gyro_x
+        self.imu_msg.orientation.y = gyro_y
+        self.imu_msg.orientation.z = gyro_z
+        self.imu_pub.publish(self.imu_msg)
 
-    def get_imu_msg(self):
-        """ 
-        Converts imu data from Robomaster SDK to ROS message
-
-        returns:
-
-        imu_msg -- ROS message for imu data 
+    def get_attitude_msg(self, attitude_info):
         """
-        acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z = self.set_imu_data()
-        imu_acc_gyro = ImuAccGyro()
-        imu_acc_gyro.acc_x = acc_x
-        imu_acc_gyro.acc_y = acc_y
-        imu_acc_gyro.acc_y = acc_y
-        imu_acc_gyro.gyro_x = gyro_x
-        imu_acc_gyro.gyro_y = gyro_y
-        imu_acc_gyro.gyro_z =gyro_z
-        return imu_acc_gyro
+        """
+        rospy.loginfo("Importing attitude data from robomaster")
+        yaw, pitch, roll = attitude_info
+        self.attitude_msg.x = yaw
+        self.attitude_msg.y = pitch
+        self.attitude_msg.z = roll
+        self.attitude_pub.publish(self.attitude_msg)
 
+    def get_esc_msg(self, esc_info):
+        """
+        """
+        rospy.loginfo("Importing esc data from robomaster")
+        speed, angle, timestamp, state = esc_info
+        self.esc_msg = speed + angle + timestamp + state
+        #self.esc_msg.timestamp = timestamp
+        #self.esc_msg.state = state
+        self.esc_pub.publish(self.esc_msg)
+
+    def get_battery_msg(self, batter_info, robot):
+        """
+        Gets battery data from Robomaster's Python SDK and
+        convers it to ROS message and then
+        publishes converted message to the relevant topic
+
+        args:
+        batter_info -- battery data that comes from robomaster's subscriber method
+        robot -- robot instance from robomaster's robot class
+        """
+        rospy.loginfo("Importing battery data from robomaster")
+        percent = batter_info
+        self.battery_msg = percent
+        self.battery_pub.publish(self.battery_msg)
+
+    def get_status_params(self, status_info):
+        """
+        """
+        rospy.loginfo("Setting status parameters for chassis from robomaster")
+        static_flag, up_hill, down_hill, on_slope, pick_up, slip_flag, impact_x, impact_y, impact_z, roll_over, hill_static = status_info
+        self.robomaster_static_flag = rospy.set_param('~robomaster/chassis/static_flag', static_flag)
+        self.robomaster_up_hill = rospy.set_param('~robomaster/chassis/up_hill', up_hill)
+        self.robomaster_down_hill = rospy.set_param('~robomaster/chassis/down_hill', down_hill)
+        self.robomaster_on_slope = rospy.set_param('~robomaster/chassis/on_slope', on_slope)
+        self.robomaster_pick_up = rospy.set_param('~robomaster/chassis/pick_up', pick_up)
+        self.robomaster_slip_flag = rospy.set_param('~robomaster/chassis/slip_flag', slip_flag)
+        self.robomaster_impact_x = rospy.set_param('~robomaster/chassis/impact_x', impact_x)
+        self.robomaster_impact_y = rospy.set_param('~robomaster/chassis/impact_y', impact_y)
+        self.robomaster_impact_z = rospy.set_param('~robomaster/chassis/impact_z', impact_z)
+        self.robomaster_roll_over = rospy.set_param('~robomaster/chassis/roll_over', roll_over)
+        self.robomaster_hill_static = rospy.set_param('~robomaster/chassis/hill_static', hill_static)
 
 if __name__ == '__main__':
-    try:
-        # Initialize ROS node
-        rospy.init_node('robomaster_talker')
+    # Initialize ROS node
+    print('Initializing robomaster_talker node')
+    rospy.init_node('robomaster_talker')
+    print('Navigating to robomaster_talker node')
+    # Take a loop until an interruption is made
+    while not rospy.is_shutdown():
         # Create an instance from talker class
         robomaster_talker = RobomasterTalker()
-        # Unless there is an interruption
-        while not rospy.is_shutdown():
-            # Publish image_msg to image topic
-            robomaster_talker.img_pub.publish(robomaster_talker.get_image_msg())
-            # Publish imu_msg to imu topic
-            robomaster_talker.imu_pub.publish(robomaster_talker.get_imu_msg())
-
-            # Set publish frequency rate
-            robomaster_talker.rate.sleep()
-            
-    except rospy.ROSInterruptException:
-        # Delete Robomaster robot instance
-        robomaster_talker.robot.close()
+    robomaster_talker.close()
